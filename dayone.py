2ccd138f4fd175f405463e77c312c718f88376d9
yaminisrikandrekula <yaminisrikandrekula@gmail.com>
2025-10-01 18:58:00 +0530

    Update dayone.py

--- dayone.py
+++ dayone.py
@@ -7,3 +7,34 @@ for i in range(11):
   print (flower[i-1])
 for i in range(1,51):
     print(i)
+
+for i in range(6):
+    print(i)
+
+j=1
+while (j < 6):
+    print(j)
+    j=j+1
+    
+    j=51
+while (j < 51):
+    print(j)
+    j=j+1
+    
+n=int(input("Enter a Number:"))
+if n%2==0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
+    
+name1=input("Enter your name")
+print(name1)
+name2=input("Enter your name")
+print(name2)
+    
+name1=input("Enter your name in a capital:")
+name2=input("Enter your name:")
+Bothnames=name1+name2
+print(Bothnames)
+small_letters=Bothnames.lower()
+print(small_letters)
