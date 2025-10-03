baf2862f26bf22752d46379f5aea7311c0d585a0
yaminisrikandrekula <yaminisrikandrekula@gmail.com>
2025-10-03 19:08:29 +0530

    Create daythree.py

---
+++ daythree.py
@@ -0,0 +1,54 @@
+a=10
+print(type(a))
+s=str(a)
+print(type(s))
+k=float(s)
+print(type(k))
+
+list=[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+k=set(list)
+print(k)
+k.add(12)
+k.add(12)
+print(k)
+
+dict={
+    "model":"1st model",
+    "year":2021
+}
+print(type(dict))
+print(dict)
+
+dict={
+    "model":"1st model",
+    "year":2021,
+    "age":25
+}
+print(type(dict))
+print(dict)
+
+def mom(a,b):
+    print(a+b)
+a=10
+b=20
+mom(a,b)
+mom(5,6)
+mom(a,b)
+mom(5,6)
+# print(type(dict))
+print(dict['model'])
+dict['city']='Hyderabad'
+print(dict)
+
+def mom(a,b):
+    print(a+b)
+a=10
+b=20
+c=mom(a,b)
+print(c)
+d=mom(5,6)
+print(d)
+e=mom(a,b)
+print(e)
+f=mom(5,6)
+print(f)
