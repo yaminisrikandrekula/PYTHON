73ecbe123bd63920c248be61e3179ca414c743af
yaminisrikandrekula <yaminisrikandrekula@gmail.com>
2025-10-04 17:29:56 +0530

    Create dayfour.py

---
+++ dayfour.py
@@ -0,0 +1,18 @@
+list=[1,2,3,4,5,6,7,8,9]
+print(list[4:8])
+
+list=[1,2,3,4,5,6,7,8,9]
+print(list[4:])
+
+list=[1,2,3,4,5,6,7,8,9]
+print(list[1::2])
+
+list=[1,2,3,4,5,6,7,8,9]
+print(list[0::3])
+
+def mom (a,b):
+    print(a+b)
+a=10
+b=20
+c=mom(a,b)
+print(c)
