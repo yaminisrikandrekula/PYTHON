cf64a70ce68e68ddb5963bd64112a546ab947884
yaminisrikandrekula <yaminisrikandrekula@gmail.com>
2025-10-04 18:00:12 +0530

    Update dayfour.py

--- dayfour.py
+++ dayfour.py
@@ -34,3 +34,16 @@ try:
     print("sum:", result)
 except valueError:
     print("Invalid input!please enter number only.")
+
+try:
+    num=int(input("Enter a number:"))
+    result = 10/num
+    print("Result:", result)
+except valueError:
+    print("Invalid input!please enter number.")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+except Exception as e:
+    print("An error occupied:",e)
+finally:
+    print("Execution finished.")
