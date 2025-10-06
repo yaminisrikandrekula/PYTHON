c891ff9bbd96ccf6ea8b3a4b86006dcbaf5dde08
yaminisrikandrekula <yaminisrikandrekula@gmail.com>
2025-10-06 18:17:44 +0530

    Update dayfive.py

--- dayfive.py
+++ dayfive.py
@@ -8,3 +8,14 @@ class Person:
 
 person= Person("AKHIL",23)
 print(person)
+
+class Dog:
+    def __init__(self,name,age):
+        self .name = name
+        self .age = age
+        
+    def __str__(self):
+        return f"{self.name},{self.age} years old"
+
+dog= Dog("MINTOO",5)
+print(dog)
